a1b8281847e3d013ac206c6da9e2d99e9ed9241b
Cristhian Hernandez <cristhianhh21@gmail.com>
2020-01-03 21:32:07 -0600

    Agregar archivos de proyecto.

---
+++ ClinicaGAP/Controllers/EstadoController.cs
@@ -0,0 +1,123 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using ClinicaGAP.Models;
+
+namespace ClinicaGAP.Controllers
+{
+    public class EstadoController : Controller
+    {
+        private ClinicaGAPEntities db = new ClinicaGAPEntities();
+
+        // GET: Estado
+        public ActionResult Index()
+        {
+            return View(db.ESTADO.ToList());
+        }
+
+        // GET: Estado/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ESTADO eSTADO = db.ESTADO.Find(id);
+            if (eSTADO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eSTADO);
+        }
+
+        // GET: Estado/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Estado/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_ESTADO,DESCRIPCION")] ESTADO eSTADO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ESTADO.Add(eSTADO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(eSTADO);
+        }
+
+        // GET: Estado/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ESTADO eSTADO = db.ESTADO.Find(id);
+            if (eSTADO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eSTADO);
+        }
+
+        // POST: Estado/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_ESTADO,DESCRIPCION")] ESTADO eSTADO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eSTADO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eSTADO);
+        }
+
+        // GET: Estado/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ESTADO eSTADO = db.ESTADO.Find(id);
+            if (eSTADO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eSTADO);
+        }
+
+        // POST: Estado/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ESTADO eSTADO = db.ESTADO.Find(id);
+            db.ESTADO.Remove(eSTADO);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ClinicaGAP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClinicaGAP.Startup))]
+namespace ClinicaGAP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
